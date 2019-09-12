14a5edf97d7fdea201ec278f7f22c499c5fd79c8
alzhab <abd.alz.b>
2019-09-12 23:10:24 +0600

    User mini card

--- src/app/shared/components/user-mini-card/user-mini-card.component.ts
+++ src/app/shared/components/user-mini-card/user-mini-card.component.ts
@@ -1,12 +1,31 @@
 import { Component, OnInit } from "@angular/core";
+import { trigger, transition, style, animate } from "@angular/animations";
 
 @Component({
   selector: "app-component-user-mini-card",
   templateUrl: "./user-mini-card.component.html",
-  styleUrls: ["./user-mini-card.component.scss"]
+  styleUrls: ["./user-mini-card.component.scss"],
+  animations: [
+    trigger("enterAnimation", [
+      transition(":enter", [
+        style({ transform: "translateX(0)", opacity: 0 }),
+        animate("300ms", style({ transform: "translateX(-100%)", opacity: 1 }))
+      ]),
+      transition(":leave", [
+        style({ transform: "translateX(-100%)", opacity: 1 }),
+        animate("300ms", style({ transform: "translateX(0)", opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class UserMiniCardComponent implements OnInit {
+  public showMenu = false;
+
   constructor() {}
 
   ngOnInit() {}
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
 }
